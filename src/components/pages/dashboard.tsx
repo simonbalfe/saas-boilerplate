c067839ab33ff3a85997790acbd7203773d2ecd7
simon <sbalfedev@gmail.com>
2025-11-30 21:32:58 +0000

    test

--- src/components/pages/dashboard.tsx
+++ src/components/pages/dashboard.tsx
@@ -32,7 +32,7 @@ export function Dashboard() {
   if (loading) {
     return (
       <div className="flex items-center justify-center min-h-screen bg-background">
-        <div className=" animate-spin rounded-full h-12 w-12 border-t-2 border-b-2 border-primary"></div>
+        <div className="animate-spin rounded-full h-12 w-12 border-t-2 border-b-2 border-primary"></div>
       </div>
     )
   }
